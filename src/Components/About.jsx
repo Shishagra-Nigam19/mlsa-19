641b50a6fc4f9b468b0b617f054a56ea8533b287
varumaa19 <121211227+varumaa19@users.noreply.github.com>
2023-10-01 16:31:42 +0530

    add changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/back.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "sunset background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Full Stack Developer and I love implementing Responsive Website and giving user interface friendly And Want to get placed in Microsoft.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -35,8 +35,9 @@ const skillsList = [
   "User experience",
   "Inclusive design",
   "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Website user interfaces",
+  "Web Development",
+  " Creativity",
 ];
 
 /**
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/lion.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Be A Like a lion king of your world";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Flight Management Website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is a website for flight Management.",
+    url: "http://shishagra-nigamfilght.github/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Portfolio Website",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "My Skills and Progress and about myself.",
+    url: "https://shishagra-nigam19.github.io/Complete-portfolio-website/",
   },
   {
-    title: "My Resume Site",
+    title: "Smart Education System",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "This Website is all about revamping the education System",
+    url: " http://shishagra-nigam19smarteducation.github/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Website to Cure Depression",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is a website where people can come and express his/her thoughts.",
+    url: "http://shishagra-nigam19curedepression.github/",
   },
 ];
 
